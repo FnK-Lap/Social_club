aa2c9e15c086abe687b3e1d3d3a0f743e33eac9d
Franck <franck.lapeyre@supinternet.fr>
2014-05-15 16:01:45 +0200

    Add statut class

---
+++ Franck/class/statut.class.php
@@ -0,0 +1,54 @@
+<?php
+
+class Statut
+{
+	private $id;
+	private $id_user;
+	private $date;
+	private $content;
+
+	/*
+	**	Getter
+	*/
+	public function get_id()
+	{
+		return $this->id;
+	}
+
+	public function get_id_user()
+	{
+		return $this->id_user;
+	}
+
+	public function get_date()
+	{
+		return $this->date;
+	}
+
+	public function get_id()
+	{
+		return $this->content;
+	}
+
+	/*
+	**	Setter
+	*/
+	public function set_id($id)
+	{
+		$this->id = $id;
+	}
+	public function set_id_user($id_user)
+	{
+		$this->id_user = $id_user;
+	}
+	public function set_date($date)
+	{
+		$this->date = $date;
+	}
+	public function set_content($content)
+	{
+		$this->content = $content;
+	}
+}
+
+?>
