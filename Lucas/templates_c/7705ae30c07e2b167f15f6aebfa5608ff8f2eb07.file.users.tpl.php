d041902d7a3674f49cb2ecf281c8f9a11a01f547
LucasAlumni <luca.95@live.fr>
2014-05-27 22:42:56 +0200

    fx

---
+++ Lucas/templates_c/7705ae30c07e2b167f15f6aebfa5608ff8f2eb07.file.users.tpl.php
@@ -0,0 +1,142 @@
+<?php /* Smarty version Smarty-3.1.18, created on 2014-05-27 19:16:00
+         compiled from "views/templates/users.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:4468048265384c829e43757-96200936%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '7705ae30c07e2b167f15f6aebfa5608ff8f2eb07' => 
+    array (
+      0 => 'views/templates/users.tpl',
+      1 => 1401210957,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '4468048265384c829e43757-96200936',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.18',
+  'unifunc' => 'content_5384c82a031f29_42408429',
+  'variables' => 
+  array (
+    'user' => 0,
+    'statut' => 0,
+    'statutContent' => 0,
+    'avatar' => 0,
+    'friends' => 0,
+    'friend' => 0,
+  ),
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5384c82a031f29_42408429')) {function content_5384c82a031f29_42408429($_smarty_tpl) {?><?php if (!is_callable('smarty_modifier_capitalize')) include '/Applications/MAMP/htdocs/Social_club/Lucas/libs/smarty/plugins/modifier.capitalize.php';
+?><header>
+	<div id='title-logo'>
+		Social<span class='title-color'>Club</span>
+	</div>
+	<div id='statut' class='title-status'>
+		Votre statut : <input type='text' class='hidden' id='statut-input'><span id='statut-span'>"<?php  $_smarty_tpl->tpl_vars['statut'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['statut']->_loop = false;
+ $_from = $_smarty_tpl->tpl_vars['user']->value->get_statuts(); if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+ $_smarty_tpl->tpl_vars['statut']->total= $_smarty_tpl->_count($_from);
+ $_smarty_tpl->tpl_vars['statut']->iteration=0;
+foreach ($_from as $_smarty_tpl->tpl_vars['statut']->key => $_smarty_tpl->tpl_vars['statut']->value) {
+$_smarty_tpl->tpl_vars['statut']->_loop = true;
+ $_smarty_tpl->tpl_vars['statut']->iteration++;
+ $_smarty_tpl->tpl_vars['statut']->last = $_smarty_tpl->tpl_vars['statut']->iteration === $_smarty_tpl->tpl_vars['statut']->total;
+?><?php $_smarty_tpl->tpl_vars['statutContent'] = new Smarty_variable($_smarty_tpl->tpl_vars['statut']->value->get_content(), null, 0);?><?php if ($_smarty_tpl->tpl_vars['statut']->last) {?><?php echo $_smarty_tpl->tpl_vars['statutContent']->value;?>
+<?php }?><?php } ?>"</span>
+	</div>
+	<div id='deconnexion'>
+		<a href='index.php?action=logout'>
+			<div class='deconnexion-bloc'>
+				<img src="images/logout.png" class='deconnexion_bloc-pict' alt='logout'>
+				<p class='deconnexion_bloc-text'>Déconnexion</p>
+			</div>
+		</a>
+	</div>
+	<a href = "javascript:void(0)" onclick = "document.getElementById('light').style.display='block';document.getElementById('fade').style.display='block'"><div class='plus-info'></div></a>
+	<div id="light" class="white_content">
+		<a href = "javascript:void(0)" onclick = "document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'" class="textright">Close</a>
+	</div>
+	<div id="fade" class="black_overlay" ></div>
+	<nav>
+		<div class='nav-menu'>
+			<ul>
+			    <a href="index.php"><li><p class='nav-title'>Ma page d'accueil</p></li></a>
+			    <a href="index.php?action=profil"><li><p class='nav-title'>Mon profil</p></li></a>
+			    <a href="index.php?action=message"><li><p class='nav-title'>Mes messages</p></li></a>
+			</ul>
+		</div>
+	</nav>
+</header>
+<aside>
+	<div id='photo_bloc'>
+		<div class='photo_profil'><img src="<?php $_smarty_tpl->tpl_vars['avatar'] = new Smarty_variable($_smarty_tpl->tpl_vars['user']->value->get_avatar(), null, 0);?><?php echo $_smarty_tpl->tpl_vars['avatar']->value->get_photo();?>
+" alt='Avatar utilisateur'></div>
+		<p class='photo-nom'><?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['user']->value->get_prenom());?>
+ <?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['user']->value->get_nom());?>
+</p>
+	</div>
+	<div class='aside-line'></div>
+	<div id='friend_bloc'>
+		<p class='title-aside'>Mes amis</p><a href="index.php?action=users"><div class='photo-plusfriends'></div></a>
+		<?php  $_smarty_tpl->tpl_vars['friend'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['friend']->_loop = false;
+ $_from = $_smarty_tpl->tpl_vars['friends']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['friend']->key => $_smarty_tpl->tpl_vars['friend']->value) {
+$_smarty_tpl->tpl_vars['friend']->_loop = true;
+?>
+			<div class='friend-user'>
+				<div class='friend-connected-user'></div>
+				<p class='friend-user-info'><?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['friend']->value->get_prenom());?>
+ <?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['friend']->value->get_nom());?>
+</p>
+			</div>
+		<?php }
+if (!$_smarty_tpl->tpl_vars['friend']->_loop) {
+?>
+			<p>Vous n'avez pas d'amis</p>
+		<?php } ?>
+	</div>
+	<div class='aside-line'></div>
+	<div id='last_message_bloc'>
+		<p class='title-aside'>Mes derniers messages</p>
+		<div class='last_message'>
+			<div class='last_message-user'>
+				<p class='last_message-user-info'>Franck Laypere<span class='last_message-date'> il y a 10 minutes</span></p>
+				<div class='last_message-content'>" Ce soir, je fini l'ORM du projet et apres on ce rejoind sur Skype ?"</div>
+			</div>
+			<div class='last_message-user'>
+				<p class='last_message-user-info'>Franck Laypere<span class='last_message-date'> il y a 10 minutes</span></p>
+				<div class='last_message-content'>" Ce soir, je fini l'ORM du projet et apres on ce rejoind sur Skype ?"</div>
+			</div>
+			<div class='last_message-user'>
+				<p class='last_message-user-info'>Franck Laypere<span class='last_message-date'> il y a 10 minutes</span></p>
+				<div class='last_message-content'>" Ce soir, je fini l'ORM du projet et apres on ce rejoind sur Skype ?"</div>
+			</div>
+			<div class='last_message-user'>
+				<p class='last_message-user-info'>Franck Laypere<span class='last_message-date'> il y a 10 minutes</span></p>
+				<div class='last_message-content'>" Ce soir, je fini l'ORM du projet et apres on ce rejoind sur Skype ?"</div>
+			</div>
+			<div class='last_message-user'>
+				<p class='last_message-user-info'>Franck Laypere<span class='last_message-date'> il y a 10 minutes</span></p>
+				<div class='last_message-content'>" Ce soir, je fini l'ORM du projet et apres on ce rejoind sur Skype ?"</div>
+			</div>
+			<div class='last_message-user'>
+				<p class='last_message-user-info'>Franck Laypere<span class='last_message-date'> il y a 10 minutes</span></p>
+				<div class='last_message-content'>" Ce soir, je fini l'ORM du projet et apres on ce rejoind sur Skype ?"</div>
+			</div>
+		</div>
+	</div>
+</aside>
+<section>
+	<div id='users-list'>
+		<div class='users_list-bloc'>
+			<div class='users_list_bloc-photo'></div>
+			<span class='users_list_bloc-name'><center>Lucas Alumni</center></span>
+			<a href='index.php'>
+				<div class='users_list_bloc-buttom'>
+					<span class='users_list_bloc-buttom-text'>Ajouter</span>
+				</div>
+			</a>
+		</div>
+	</div>
+</section><?php }} ?>
